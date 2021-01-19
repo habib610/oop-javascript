59972f07d29d589bb46a2368728de5c32bc91345
habib610 <habib54562@gmail.com>
2021-01-19 21:09:04 +0600

    method-chaining

--- object.js
+++ object.js
@@ -2,18 +2,25 @@ class User {
 
     constructor(name, email) {
         this.name = name,
-        this.email = email
+        this.email = email,
+        this.score = 0
     }
     login(){
         console.log(`${this.name} just login`);
+        return this;
     }
     logout() {
-        console.log(`${this.name} just log out`)
+        console.log(`${this.name} just log out`);
+        return this;
+    }
+    updateScore() {
+        this.score++;
+        console.log(`${this.name} has score ${this.score}`)
+        return this;
     }
 }
 
 const userOne = new User("habib", "habib@example.com");
 const userTwo = new User("rahman", "rahman@example.com");
 
-userOne.login();
-userTwo.login()
+userOne.login().updateScore().updateScore().logout().login().updateScore().logout();
